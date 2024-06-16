c2ba02a6c0b18a4c5aa822969e3853fec7c815e0
Akshay-1001 <pichaisundar318@gmail.com>
2024-06-16 20:06:46 +0530

    reverting merge

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';  
 
+// import MailApp from './components/auth';
+// import Inbox from './components/Inbox/index.js';    
+// import SentSection from './components/sent/index.js';
+// import draftsSection from './components/drafts/index.js';
+// import starredSection from './components/starred/index.js';
+
+import Navbar from './components/nav-bar/index.js';
+import BurgerMenu from './components/burger-menu/index.js';
+import './index.css';
+
+import { BaseProvider, LightTheme } from 'baseui';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App/>);
+root.render(<React.StrictMode>        
+    <StyletronProvider value={new Styletron()}>
+    <BaseProvider theme={LightTheme}>
+        <main>
+        <section className='TopPart'>
+            <Navbar />
+        </section>
+        <section className='bottomPart'>
+            <BurgerMenu/>
+        </section>
+        </main>
+    </BaseProvider> 
+    </StyletronProvider>
+</React.StrictMode>
+);
