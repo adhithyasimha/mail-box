68a8a9236be17dfc76e435c80a182ea0dbc50f5d
Akshay-1001 <pichaisundar318@gmail.com>
2024-06-17 10:46:09 +0530

    made a navigation logic

--- src/components/burger-menu/index.js
+++ src/components/burger-menu/index.js
@@ -1,56 +1,105 @@
 import React from 'react';
+
 import './styles.css';
+
 import { Button } from "baseui/button";
 import { Plus } from 'baseui/icon';
 import { Navigation } from "baseui/side-navigation";
+
 import './ComposeBox.css';
 import ComposeBox from './ComposeBox';
 
-const BurgerMenu = ({nav, activeItemId, setActiveItemId}) => {
+import InboxComponent from '../inbox/inboxComponent.js';
+import StarredComponent from '../starred/starredComponent.js';
+import SentComponent from '../sent/sentComponent.js';
+import DraftsComponent from '../drafts/draftsComponent.js';
+ 
+
+const BurgerMenu = () => {
+  const [activeItemId, setActiveItemId] = React.useState("#inbox");
   const [isComposeOpen, setIsComposeOpen] = React.useState(false);
+  const nav = [
+    {
+      title: 'Inbox',
+      itemId: '#inbox',
+    },
+    {
+      title: 'Starred',
+      itemId: '#starred',
+    },
+    {
+      title: 'Sent',
+      itemId: '#sent',
+    },
+    {
+      title: 'Drafts',
+      itemId: '#drafts',
+    }
+  ];
 
   const composeButtonClickHandler = () => {
-    setIsComposeOpen(!isComposeOpen);
+    setIsComposeOpen(true);
   };
 
   const handleCloseCompose = () => {
     setIsComposeOpen(false);
   };
-
   return (
-    <div className="burgerMenu">
-      <div className='composeSec'>
-        <Button
-          startEnhancer={() => <Plus size={24} />}
-          overrides={{
-            BaseButton: {
-              style: ({ $theme }) => {
-                return {
-                  color: $theme.colors.primaryA,
-                  color: $theme.colors.mono100,
-                  width: '80%',
-                  ':hover': {
-                    backgroundColor: $theme.colors.primaryA,
-                  },
-                };
+    <section className="main-navigation">
+      <aside className="burgerMenu">
+        <div className='composeSec'>
+          <Button
+            startEnhancer={() => <Plus size={24} />}
+            overrides={{
+              BaseButton: {
+                style: ({ $theme }) => {
+                  return {
+                    color: $theme.colors.primaryA,
+                    color: $theme.colors.mono100,
+                    width: '80%',
+                    ':hover': {
+                      backgroundColor: $theme.colors.primaryA,
+                    },
+                  };
+                },
               },
-            },
-          }}
-          onClick={composeButtonClickHandler}
-        >
-          Compose
-        </Button>
-      </div>
-      <div className='menuSec'>
-        <Navigation
-          items={nav}
-          activeItemId={activeItemId}
-          onChange={({ item }) => setActiveItemId(item.itemId)}
-        />
-      </div>
-      {isComposeOpen && <ComposeBox onClose={handleCloseCompose} />}
-    </div>
+            }}
+            onClick={composeButtonClickHandler}
+          >
+            Compose
+          </Button>
+        </div>
+        <div className='menuSec'>
+          <Navigation
+            items={nav}
+            activeItemId={activeItemId}
+            onChange={({ item }) => {
+              setActiveItemId(item.itemId);
+              console.log(item.itemId);
+            }}
+
+          />
+        </div>
+        {isComposeOpen && <ComposeBox onClose={handleCloseCompose} />}
+      </aside>
+      <aside className='contentSec'>
+          {(()=> {
+            console.log(activeItemId);
+            switch(activeItemId){
+              case '#inbox':
+                return <InboxComponent />;
+              case '#starred':
+                return <StarredComponent />;
+              case '#sent':
+                return <SentComponent />;
+              case '#drafts':
+                return <DraftsComponent />;
+              default:
+                return <InboxComponent />;
+            }
+          })()}
+        </aside>
+    </section>
   );
 };
-
 export default BurgerMenu;
