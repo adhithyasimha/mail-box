8e4a86e9c2847dc489b7cbaeab568a5bb983968f
Akshay-1001 <pichaisundar318@gmail.com>
2024-06-19 23:19:49 +0530

    added logic/features for the table

--- src/components/sent/sentComponent.js
+++ src/components/sent/sentComponent.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Table } from 'baseui/table';
+import { PLACEMENT, StatefulPopover } from 'baseui/popover';
+import { Block } from 'baseui/block';
+import { Button } from 'baseui/button';
+import { ChevronLeft } from 'baseui/icon';
 
 const SentSection = () => {
   const [sentMails, setSentMails] = useState([]);
+  const [selectedMail, setSelectedMail] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:3001/api/supabase-sent')
@@ -12,10 +17,47 @@ const SentSection = () => {
   }, []);
 
   const columns = ['To', 'Subject', 'Message', 'Time'];
-  const data = sentMails.map(mail => [mail.to_email, mail.subject, mail.message, new Date(mail.sent_at).toLocaleString()])
+  const renderCell = (value, mail) => (
+    <Block 
+      whiteSpace='nowrap'
+      maxWidth='200px'
+      textOverflow='ellipsis'
+      overflow='hidden'
+      onClick={() => setSelectedMail(mail)}
+      overrides={{
+        Block: {
+          style: {
+            cursor: 'pointer'
+          }
+        }
+      
+      }}>
+        {value}        
+    </Block>
+  );
 
   return (
-    <Table columns={columns} data={data} />
+    <>
+    {selectedMail ? (
+      <div>
+        <Button onClick={()=>setSelectedMail(null)}><ChevronLeft size={24}/></Button>
+        <h2><strong>Subject:</strong> {selectedMail.subject}</h2>
+        <p><strong>From:</strong> {selectedMail.from_email}</p>
+        <p><strong>To:</strong> {selectedMail.to_email}</p>
+        <p><strong>Message:</strong> {selectedMail.message}</p>
+        <p><strong>Time Sent:</strong> {new Date(selectedMail.sent_at).toLocaleString()}</p>
+      </div>
+    ) : (
+      <Table columns={columns} data={sentMails
+        .sort((a, b) => new Date(b.sent_at) - new Date(a.sent_at))
+        .map((mail, index) => [
+        renderCell(mail.to_email, mail),
+        renderCell(mail.subject, mail),
+        renderCell(mail.message, mail),
+        new Date(mail.sent_at).toLocaleString() 
+      ])} />
+    )}
+    </>
     // <table id="sent">
     //   <thead>
     //     <tr>
