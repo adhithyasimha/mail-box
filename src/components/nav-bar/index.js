8aa60a3f340043e165280fb45ce5daae94422824
adhithyasimha <adhithyasimhaa@gmail.com>
2024-06-25 23:43:41 +0530

    final fix

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Avatar } from 'baseui/avatar';
 import { Search } from 'baseui/icon';
 import { StatefulInput } from 'baseui/input';
-<<<<<<< HEAD
 import { Button } from 'baseui/button';
 import gsap from 'gsap';
 import { createClient } from '@supabase/supabase-js';
@@ -17,39 +16,19 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 const Navbar = ({ onLogout }) => {
   const [value, setValue] = useState('');
   const [results, setResults] = useState([]);
-=======
-
-// GSAP Animation 
-import { useEffect, useRef } from 'react';
-import gsap from 'gsap';
-
-const Navbar = () => {
-  const [value, setValue] = React.useState('');
->>>>>>> e701268facd8f97c6e41d9436001b180c5c75086
 
   // GSAP Animation
+  const logoRef = useRef(null);
+  const searchRef = useRef(null);
   const avatarRef = useRef(null);
   const navbarRef = useRef(null); 
 
   const width = window.screen.width;
 
   useEffect(() => {
-<<<<<<< HEAD
     gsap.fromTo(logoRef.current, { y: -30 }, { y: 0, duration: 1 });
     gsap.fromTo(avatarRef.current, { x: 50 }, { x: 0, duration: 2 });
     gsap.fromTo(searchRef.current, { width: 0 }, { width: 500, duration: 1.6 });
-=======
-    gsap.fromTo(
-      avatarRef.current,
-      { x: 50 }, 
-      { x: 0, duration: 2}
-    );
-    gsap.fromTo(
-      navbarRef.current,
-      { width: 700 },
-      { width: width, duration: 2.6}
-    )
->>>>>>> e701268facd8f97c6e41d9436001b180c5c75086
   }, []);
 
   const debounce = (func, delay) => {
@@ -60,7 +39,6 @@ const Navbar = () => {
     };
   };
 
-<<<<<<< HEAD
   const handleSearch = debounce(async (query) => {
     if (query.length > 2) {
       try {
@@ -100,15 +78,6 @@ const Navbar = () => {
         <div className='logo-icon'>
           <svg width="50" height="50" viewBox="0 0 200 203" fill="none" xmlns="http://www.w3.org/2000/svg">
           <g clip-path="url(#clip0_163_237)">
-=======
-<nav className="navbar"
-  ref={navbarRef}>
-  <div className="navbar-left logo" >
-    {/* aspect ration is 1:1.2112 */}
-    <div className='logo-icon' >
-    <svg width="50" height="50" viewBox="0 0 200 203" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_163_237)">
->>>>>>> e701268facd8f97c6e41d9436001b180c5c75086
 <path d="M100.249 21L21 70.3226L100.249 120L180 70.3226L100.249 21Z" stroke="#0F0F0E"/>
 <path d="M100.385 48L64 70.4194L100.385 93L137 70.4194L100.385 48Z" stroke="#0F0F0E"/>
 <path d="M100.5 48V93" stroke="#0F0F0E"/>
@@ -120,7 +89,6 @@ const Navbar = () => {
 </g>
 <defs>
 </defs>
-<<<<<<< HEAD
           </svg>
         </div>
         <div className='logo-text'>Mail Box</div>
@@ -161,49 +129,6 @@ const Navbar = () => {
       </div>
     </nav>
   );
-=======
-</svg>
-
-    </div>
-    <div className='logo-text'>
-      Mail Box
-    </div>
-  </div>
-  <div className="search-bar">
-    <StatefulInput
-      startEnhancer={<Search size={24} />}
-      placeholder="Search for mail"
-      clearOnEscape
-
-      overrides={{
-        Input: {
-          style: ({ $theme }) => {
-            return {
-              fontFamily: 'Uber Move, sans-serif',
-            }
-          }
-        }
-      }}
-    />
-  </div>
-  <div className="navbar-center">
-    <ul className="nav-links">
-      <li>
-        <a>Help</a>
-      </li>
-    </ul>
-  </div>
-  <div className="navbar-right" 
-    ref={avatarRef}>
-    <Avatar 
-      name="Adolf Hitler"
-      size="scale1000"
-      src="https://images.uncyclomedia.co/uncyclopedia/en/5/58/Adolf.gif"
-    />
-  </div>
-</nav>
-);
->>>>>>> e701268facd8f97c6e41d9436001b180c5c75086
 };
 
 export default Navbar;
